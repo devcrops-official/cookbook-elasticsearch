7cb55c9a7a2d6959c353f90a8dd51654e0260fe8
Pierluigi Dell'Arciprete <devcrops@devcrops.org>
2019-04-07 22:02:50 +0200

    Update yum and apt repository config
    In this way it will be possible to customize the repositories urls

--- libraries/provider_install.rb
+++ libraries/provider_install.rb
@@ -220,16 +220,16 @@ class ElasticsearchCookbook::InstallProvider < Chef::Provider::LWRPBase
 
   def yum_repo_resource
     yum_repository 'elastic-6.x' do
-      baseurl 'https://artifacts.elastic.co/packages/6.x/yum'
-      gpgkey 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+      baseurl "#{node['elasticsearch']['packages']['host']}/packages/6.x/yum"
+      gpgkey "#{node['elasticsearch']['packages']['host']}/GPG-KEY-elasticsearch"
       action :nothing # :add, remove
     end
   end
 
   def apt_repo_resource
     apt_repository 'elastic-6.x' do
-      uri 'https://artifacts.elastic.co/packages/6.x/apt'
-      key 'https://artifacts.elastic.co/GPG-KEY-elasticsearch'
+      uri "#{node['elasticsearch']['packages']['host']}/packages/6.x/apt"
+      key "#{node['elasticsearch']['packages']['host']}/GPG-KEY-elasticsearch"
       components ['main']
       distribution 'stable'
       action :nothing # :create, :delete
